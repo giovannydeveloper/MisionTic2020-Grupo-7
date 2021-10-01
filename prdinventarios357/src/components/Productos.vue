758407945d728d3ce853385e3a7dcd8c2eae9e31
giovannydeveloper <49663826+giovannydeveloper@users.noreply.github.com>
2021-09-30 23:14:09 -0500

    30092021

---
+++ prdinventarios357/src/components/Productos.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+<script>
+export default {
+  name: "Menuppal",
+  props: {
+    msg: String,
+  },
+};
+</script > 
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+h1 {
+  margin: 40px 0 0;
+}
+</style> 
